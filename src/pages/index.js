269076c0ccb5f6484a4e93a134743c312902a788
David Thorpe <davidthorpe878@gmail.com>
2019-04-03 21:47:41 +0100

    First blog test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,32 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default ({ data }) => {
+  const { allMarkdownRemark } = data
+  const { edges } = allMarkdownRemark
+  return (
+    <div>
+      {edges.map(item => (
+        <p>{item.node.frontmatter.title}</p>
+      ))}
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            thumbnail
+            date
+            notes
+          }
+        }
+      }
+    }
+  }
+`
