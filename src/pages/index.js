1698771e4fb582911ed8f25c9d929c9a1a90faeb
David Thorpe <davidthorpe878@gmail.com>
2019-04-04 21:11:20 +0100

    update index links

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,15 @@ import { graphql, Link } from "gatsby"
 export default ({ data }) => {
   const { allMarkdownRemark } = data
   const { edges } = allMarkdownRemark
+
   return (
     <div>
-      {edges.map(item => (
-        <Link href={item.node.fields.slug}>{item.node.frontmatter.name}</Link>
-      ))}
+      {edges.map(item => {
+        console.log(item.node.fields.slug)
+        return (
+          <Link to={item.node.fields.slug}>{item.node.frontmatter.name}</Link>
+        )
+      })}
     </div>
   )
 }
